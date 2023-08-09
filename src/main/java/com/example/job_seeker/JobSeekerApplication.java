7c99ef0223f8b049da6685d3cd2041db6795184a
KAVI <kavindu123450@gmail.com>
2023-08-09 12:44:55 +0530

    create backend

---
+++ src/main/java/com/example/job_seeker/JobSeekerApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_seeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/job_seeker/JobSeekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_seeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
