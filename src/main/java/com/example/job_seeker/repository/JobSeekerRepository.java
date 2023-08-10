e6d7ebaba4bb8a3d8c0cde9b4c2a88accc67d20f
KAVI <kavindu123450@gmail.com>
2023-08-10 14:47:18 +0530

    Add crud operations to job_seeker

--- src/main/java/com/example/job_seeker/repository/JobSeekerRepository.java
+++ src/main/java/com/example/job_seeker/repository/JobSeekerRepository.java
@@ -1,4 +1,7 @@
 package com.example.job_seeker.repository;
 
-public interface JobSeekerRepository {
+import com.example.job_seeker.model.JobSeeker;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JobSeekerRepository extends MongoRepository<JobSeeker, Integer> {
 }
--- src/main/java/com/example/job_seeker/resource/JobSeekerController.java
+++ src/main/java/com/example/job_seeker/resource/JobSeekerController.java
@@ -1,4 +1,43 @@
 package com.example.job_seeker.resource;
 
+import com.example.job_seeker.model.JobSeeker;
+import com.example.job_seeker.repository.JobSeekerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class JobSeekerController {
+    @Autowired
+    private JobSeekerRepository jobSeekerRepository;
+
+    @PostMapping("/saveJobSeeker")
+    public String saveJobSeeker(@RequestBody JobSeeker jobSeeker) {
+        jobSeekerRepository.save(jobSeeker);
+        return "Saved "+jobSeeker.getName()+"successfully";
+    }
+
+    @GetMapping("/getAllJobSeekers")
+    public List<JobSeeker> getAllJobSeekers() {
+        return jobSeekerRepository.findAll();
+    }
+
+    @GetMapping("/getJobSeekerById/{id}")
+    public Optional<JobSeeker> getJobSeekerById(@PathVariable int id) {
+        return jobSeekerRepository.findById(id);
+    }
+
+    @DeleteMapping("/deleteJobSeeker/{id}")
+    public String deleteJobSeeker(@PathVariable int id) {
+        jobSeekerRepository.deleteById(id);
+        return "Deleted "+id+" successfully";
+    }
+
+    @PutMapping("/updateJobSeeker/{id}")
+    public String updateJobSeeker(@PathVariable int id, @RequestBody JobSeeker jobSeeker) {
+        jobSeekerRepository.save(jobSeeker);
+        return "Updated "+id+" successfully";
+    }
 }
