1aff7f4af4f3dd1190d6db0cd30d5ab1e6810f1c
KAVI <kavindu123450@gmail.com>
2023-08-12 18:59:51 +0530

    successfully integrated the load consultants for interview req page and list of consultant page with backend.

--- src/app/components/reception/reception-consultant-list/reception-consultant-list.component.ts
+++ src/app/components/reception/reception-consultant-list/reception-consultant-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {consultants} from "../../../shared/components/store/consulant.data.store.test";
+import {HttpClient} from "@angular/common/http";
+import {ConsultantService} from "../../../services/consultant.service";
 
 @Component({
   selector: 'app-reception-consultant-list',
   templateUrl: './reception-consultant-list.component.html',
   styleUrls: ['./reception-consultant-list.component.scss']
 })
-export class ReceptionConsultantListComponent {
+export class ReceptionConsultantListComponent implements OnInit{
 
-  consultants:any = consultants;
+  consultants:any;
+
+  constructor(private http: HttpClient, private consultantService: ConsultantService) {
+  }
+
+  ngOnInit() {
+    this.loadConsultants()
+  }
+
+  loadConsultants() {
+    this.consultantService.getAllConsultants().subscribe((data: any) => {
+      this.consultants = data;
+    })
+  }
+
 }
--- src/app/components/reception/reception-interview-req/reception-interview-req.component.ts
+++ src/app/components/reception/reception-interview-req/reception-interview-req.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {consultants} from "../../../shared/components/store/consulant.data.store.test";
+import {HttpClient} from "@angular/common/http";
+import {ConsultantService} from "../../../services/consultant.service";
 
 @Component({
   selector: 'app-reception-interview-req',
   templateUrl: './reception-interview-req.component.html',
   styleUrls: ['./reception-interview-req.component.scss']
 })
-export class ReceptionInterviewReqComponent {
-  consultants:any = consultants;
+export class ReceptionInterviewReqComponent implements OnInit{
+  consultants:any;
+
+  constructor(private http: HttpClient, private consultantService: ConsultantService) {
+  }
+
+  ngOnInit() {
+    this.loadInterviewReqs();
+  }
+
+  loadInterviewReqs() {
+    this.consultantService.getAllConsultants().subscribe((data: any) => {
+      this.consultants = data;
+    })
+  }
 
 }
