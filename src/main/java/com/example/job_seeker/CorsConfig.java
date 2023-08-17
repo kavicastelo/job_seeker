4fbd5b5fa41de525753a1d134d9b71dd506e822a
KAVI <kavindu123450@gmail.com>
2023-08-17 16:39:39 +0530

    change cors config

--- src/main/java/com/example/job_seeker/CorsConfig.java
+++ src/main/java/com/example/job_seeker/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1") // Specify the mapping pattern you want to allow CORS for
-                .allowedOrigins("http://localhost:4200") // Allow requests from this origin
+//                .allowedOrigins("http://localhost:4200") // Allow requests from this origin uncomment this line to run on localhost
+                .allowedOrigins("https://seeker-by-kavi.netlify.app") // Allow requests from this origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow sending credentials (cookies, authentication headers)
@@ -25,7 +26,8 @@ public class CorsConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
+//        config.addAllowedOrigin("http://localhost:4200"); // uncomment this line to run on localhost
+        config.addAllowedOrigin("https://seeker-by-kavi.netlify.app");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
